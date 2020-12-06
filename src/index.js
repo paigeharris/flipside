8c47e79c308a5a3d352360cca77abc368b2905b3
paige <impaigeharris@gmail.com>
2020-12-06 16:14:53 -0700

    Import and use Material UI + theme

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import theme from "./styles/theme";
+import {ThemeProvider} from '@material-ui/core';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <ThemeProvider theme={theme}>
+          <App />
+      </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
