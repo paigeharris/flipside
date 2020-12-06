98cd63d6d8a4831291251b89ced5a3bc61c4696c
paige <impaigeharris@gmail.com>
2020-12-06 16:34:47 -0700

    Setup basic MUI layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import MenuRounded from '@material-ui/icons/MenuRounded';
+
+import {
+    Root,
+    Header,
+    Nav,
+    Content,
+    Footer,
+    presets,
+} from 'mui-layout';
+
+const config = presets.createStandardLayout();
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Root config={config}>
+            <Header renderMenuIcon={open => (open ? <ChevronLeft/> : <MenuRounded/>)}>
+                header
+            </Header>
+            <Nav renderIcon={collapsed =>
+                    collapsed ? <ChevronRight/> : <ChevronLeft/>
+                }>
+                nav
+            </Nav>
+            <Content>
+                content
+            </Content>
+            <Footer>
+                footer
+            </Footer>
+        </Root>
+    );
 }
 
 export default App;
